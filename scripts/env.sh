9748c7756912ae3d2b8afe014e098a231a35ba23
wangshaocong92 <1635573386@qq.com>
2026-03-03 17:51:06 +0800

    feat: update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,4 +1,5 @@
 export PROJECT_PATH=$(cd $(dirname $0)/..; pwd)
-export TRT_COOKBOOK_PATH=$PROJECT_PATH"/doc/trt_samples/cookbook"
-export PYTHONPATH=$PROJECT_PATH"/doc/trt_samples/cookbook:$PYTHONPATH"
-export LD_LIBRARY_PATH="/usr/local/cuda/targets/x86_64-linux/lib":$LD_LIBRARY_PATH
+export CUDA_HOME=/usr/local/cuda-12 
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
