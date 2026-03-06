cdaa2076ba482cbed9f32b179ddfd15fb604ceee
wangshaocong92 <wangshaocong@megvii.com>
2026-03-06 13:54:36 +0800

    feat: update cute doc

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,13 @@ source $(cd $(dirname $0)/../; pwd)/scripts/env.sh
 
 export BUILD_PATH=$PROJECT_PATH/build
 if [ ! -d $BUILD_PATH ]; then
-    mdkir -p $BUILD_PATH
+    mkdir -p $BUILD_PATH
 else
     rm -rf $BUILD_PATH/*
 fi
 
 cd $BUILD_PATH
-conan install $PROJECT_PATH --build=missing
+conan install $PROJECT_PATH --build=missing -deployer-folder=$BUILD_PATH
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$(pwd) -DCMAKE_VERBOSE_MAKEFILE=ON  -DCMAKE_INSTALL_PREFIX=$PROJECT_PATH/install $PROJECT_PATH
 ninja -j$(nproc)
 ninja install
