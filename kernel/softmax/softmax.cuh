a6fb1909e22dce2d08a9e1fc64160a7e257c675c
wangshaocong92 <1635573386@qq.com>
2026-07-09 10:08:27 +0000

    feat:update kernel test

--- kernel/softmax/softmax.cuh
+++ kernel/softmax/softmax.cuh
@@ -1,13 +1,24 @@
 #pragma once
+#include <cutlass/cutlass.h>
 
-
+#include <cute/tensor.hpp>
+#include <cuda_runtime_api.h>
+#include "cute/numeric/integral_constant.hpp"
 namespace kernel {
     namespace gpu {
-        void softmax_forward(const float *input, float *output, int n)
+        /*
+            输入是一个一维数组，长度为 n，在 GPU 上计算 softmax 的前向传播，在原地输出，长度为 n。
+            1. 使用多少个block，单个block的线程数为多少
+            2. block 内做reduce
+
+
+        */
+        void __global__ softmax_forward(cute::Tensor<float, cute::Shape<int>> &input)
         {
+            //// 获取 block 和 thread 的索引
 
         }
-        void online_softmax_forward(const float *input, float *output, int n)
+        void __global__ online_softmax_forward(const float *input, float *output, int n)
         {
 
         }
